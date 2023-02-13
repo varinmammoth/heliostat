9fdb69e30ffe69372f0c7776ac6eeaeba08c5b46
Skywatcher90 <47398576+Skywatcher90@users.noreply.github.com>
2023-02-13 16:22:37 +0000

    The new update contains only minor changes in some comments.

--- Matrix.py
+++ Matrix.py
@@ -29,27 +29,30 @@ def mirror_normal(input, reflected):
     # we need to divide the angle by 2 (law of reflections)
 
     mirror_normal = np.array(rotation(angle, rotation_axis)@receiver)[0]
+
+    rotation_test = np.array(rotation(angle * 2, rotation_axis)@receiver)[0]
+    print('MIRROR NORMAL=', mirror_normal)
     # finally, the mirror normal is the reflected ray rotated by the angle around the rot. axis
 
-    print('ROTATION AXIS=' ,rotation_axis)
-    print('SUN DIRECTION=',sun)
-    print('RECEIVER DIRECTION=' ,receiver)
-    print('ROTATION ANGLE=',angle)
-    print('MIRROR NORMAL=',mirror_normal)
+    #print('ROTATION AXIS=' ,rotation_axis)
+    #print('SUN DIRECTION=',sun)
+    #print('RECEIVER DIRECTION=' ,receiver)
+    #print('ROTATION ANGLE=',angle)
+    #print('MIRROR NORMAL=',mirror_normal)
+    #print()
 
-    soa = np.array([[0, 0, 0, sun[0], sun[1], sun[2]], [0, 0, 0, receiver[0], receiver[1], receiver[2]],
-                    [0, 0, 0, mirror_normal[0], mirror_normal[1], mirror_normal[2]], [0, 0, 0, rotation_axis[0], rotation_axis[1], rotation_axis[2]]])
 
-    X, Y, Z, U, V, W = zip(*soa)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.quiver(X, Y, Z, U, V, W)
-    ax.set_xlim([-0.5, 0.5])
-    ax.set_ylim([-0.5, 0.5])
-    ax.set_zlim([-0.5, 0.5])
-    plt.show()
 
-input = np.array([-1, -7, 1])
-reflected = np.array([1, -4, 1])
+    # soa = np.array([[0, 0, 0, sun[0], sun[1], sun[2]], [0, 0, 0, receiver[0], receiver[1], receiver[2]],
+    #                 [0, 0, 0, mirror_normal[0], mirror_normal[1], mirror_normal[2]], [0, 0, 0, rotation_axis[0], rotation_axis[1], rotation_axis[2]]])
+    #
+    # X, Y, Z, U, V, W = zip(*soa)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.quiver(X, Y, Z, U, V, W)
+    # ax.set_xlim([-1, 1])
+    # ax.set_ylim([-1, 1])
+    # ax.set_zlim([-1, 1])
+    # plt.show()
 
-mirror_normal(input, reflected)
+    return mirror_normal
