3fca90bba110c1671151585c2e02705296f39e71
varinmammoth <88394052+varinmammoth@users.noreply.github.com>
2023-03-06 22:54:02 +0000

    Set up code ready to run and get final results for longest day in London.

--- analysis2.py
+++ analysis2.py
@@ -8,9 +8,13 @@ from numba import typed
 from numba import int32, int64, float64
 from scipy.interpolate import CubicSpline
 from scipy.integrate import odeint, quad
+from scipy.stats import chisquare
 
 from playground_fast import *
 from initialization_code_fast import *
+
+plt.rc('font', size=13)
+plt.rc('axes', labelsize=13)
 # %%
 
 def performance(position_ls, receiver_pos, mirror_dim, receiver_dim, phi_ls, theta_ls, earth_sun_distance_ls, light_cone=True, N=10):
@@ -413,3 +417,200 @@ plt.ylim([0,2000])
 plt.show()
 
 # %%
+'''
+Categorizing uncertainty from using various number of points
+'''
+ray_density = 150
+ground_length = 30
+mirror_num_ls = [4,8,16,32,64]
+mirror_dim = [1.,1.]
+receiver_dim = [1.,1.,1.]
+receiver_pos = [0.,0.,15.]
+
+R_list = [10]
+
+t_scenario_ls2 = []
+power_scenario_ls2 = []
+count_scenario_ls2 = []
+
+day = ts.utc(2014, 12, 25) #Date
+lat = '13.7563 N' #Location
+long = '100.5018 E'
+elevation = 1.5 #Elevation
+
+N_points_ls = [3, 5, 10, 15, 20, 25]
+#%%
+start = time.time()
+for i, N_points in enumerate(N_points_ls):
+    t, t_sunrise, phi, theta, distance = get_solar_positions(day, lat, long, elevation, N_points)
+
+    for j, val in enumerate(theta):
+        if val < 0:
+            theta[j] = 0
+
+    for radius in R_list:
+        position_ls = create_circular_positions(radius, mirror_num_ls)
+        t_out, power_ls, count_ls = performance_no_cone_2step(t_sunrise, position_ls, receiver_pos, mirror_dim,receiver_dim,phi,theta,ray_density,ground_length)
+        power_scenario_ls2.append(power_ls)
+        count_scenario_ls2.append(count_ls)
+        t_scenario_ls2.append(t_sunrise)
+
+    print(i+1)
+   
+end = time.time()
+print(end-start)
+# %%
+#Get the time list (delete later, forgot to put it in above)
+t_scenario_ls2 = []
+for i, N_points in enumerate(N_points_ls):
+    t, t_sunrise, phi, theta, distance = get_solar_positions(day, lat, long, elevation, N_points)
+    t_scenario_ls2.append(t_sunrise)
+
+#Interpolate each situation and find the chi-square with the previous scenario
+power_interp_ls = []
+t_ls = np.linspace(0, t_sunrise[-1], 100)
+plt.figure(figsize=(7,5), dpi=800)
+for i, power_ls in enumerate(power_scenario_ls2):
+    func = CubicSpline(t_scenario_ls2[i], power_ls)
+    def func2(t):
+        if type(t) == np.ndarray or type(t) == list:
+            output = []
+            for i in t:
+                output.append(max(0, func(i)))
+            return np.array(output)
+        else:
+            return max(0, func(t))
+    power_interp_ls.append(func)
+    plt.plot(t_ls, func2((t_ls)), label=f'{N_points_ls[i]}')
+plt.legend(title=r'$N_{interp.}$')
+plt.xlabel('Time since sunrise (s)')
+plt.ylabel('Power (arbitrary units)')
+ax = plt.gca()
+temp = ax.xaxis.get_ticklabels()
+temp = list(set(temp) - set(temp[::2]))
+for label in temp:
+    label.set_visible(False)
+plt.show()
+#%%
+# Now compute the chi square
+t_ls = np.linspace(0, t_sunrise[-1], 100)
+chi_ls = []
+for i in range(1, len(power_interp_ls)):
+    chi_score = np.sum((power_interp_ls[i](t_ls) - power_interp_ls[i-1](t_ls))**2)/100**2
+    chi_ls.append(chi_score)
+
+plt.figure(figsize=(7,5), dpi=800)
+plt.plot(N_points_ls[0:-1], chi_ls, '.', c='black', markersize=8)
+plt.xlabel('Number of points on P. vs. t graph')
+plt.ylabel(r'$\frac{(P_{i}-P_{i-1})^{2}}{N_{sample}}$')
+plt.show()
+# %%
+'''
+Get final results
+Longest day in London
+'''
+day = ts.utc(2022, 6, 21) #Date
+lat = '51.5072 N' #Location
+long = '0.1276 W'
+elevation = 1.5 #Elevation
+t, t_sunrise, phi, theta, distance = get_solar_positions(day, lat, long, elevation, 20)
+
+for i, val in enumerate(theta):
+    if val < 0:
+        theta[i] = 0
+#%%
+ray_density = 150
+ground_length = 30
+mirror_num_ls = [4,8,16,32,64,128]
+mirror_dim = [1.,1.]
+receiver_dim = [1.,1.,1.]
+receiver_pos = [0.,0.,15.]
+
+#Performance of same mirror config, but placing the mirrors farther from receiver/from each other
+R_list = [6, 7, 8, 9, 10, 11, 12, 13, 14]
+power_scenario_ls = []
+count_scenario_ls = []
+
+start = time.time()
+for radius in R_list:
+    position_ls = create_circular_positions(radius, mirror_num_ls)
+    t_out, power_ls, count_ls = performance_no_cone_2step(t_sunrise, position_ls, receiver_pos, mirror_dim,receiver_dim,phi,theta,ray_density,ground_length)
+    power_scenario_ls.append(power_ls)
+    count_scenario_ls.append(count_ls)
+end = time.time()
+print(f'Elapsed time: {end-start}')
+#%%
+#Ground power
+ground_power_ls = ground_power(ray_density, theta, phi, ground_length)
+
+#Estimate predictions
+# from estimate_predictions import *
+ground_power_pred_ls = ground_power_pred(ray_density, theta)
+mirror_power_upperbound_ls = mirror_power_pred(ray_density, ground_length**2, mirror_dim[0]*mirror_dim[1], np.sum(mirror_num_ls), theta)
+#%%
+#Plot the result
+plt.clf()
+plt.figure(dpi=800)
+t = np.linspace(0, t_sunrise[-1], 40000)
+for radius_i, radius in enumerate(R_list):
+    plt.plot(t_sunrise, power_scenario_ls[radius_i], '.', color="C{}".format(radius_i))
+    func = CubicSpline(t_sunrise, power_scenario_ls[radius_i])
+    plt.plot(t, func(t), color="C{}".format(radius_i), label=f'R={radius}')
+
+plt.plot(t_sunrise, ground_power_ls, '.', label='Total power incident on area (Simulated)')
+plt.plot(t_sunrise, ground_power_pred_ls, '--', alpha=0.5, label='Total power incident on area (Predicted)')
+plt.plot(t_sunrise, mirror_power_upperbound_ls, '--', alpha=0.5, label='Estimated power collected by receiver (Predicted)')
+
+plt.xlabel('''Time since sunrise (s)
+
+R = Total radius of mirror configuration
+''')
+plt.ylabel('Power (Arbitrary units)')
+plt.legend(bbox_to_anchor =(0.5,-0.63), loc='lower center')
+
+plt.ylim([-100,3000])
+plt.show()
+#%%
+'''
+From power, get temperature
+'''
+Tc = 300
+Tfunc_ls = []
+Tavg_ls = []
+Terrfunc_ls = []
+for power in power_scenario_ls:
+    power = np.array(power)
+
+    t_test = np.linspace(t_sunrise[0], t_sunrise[-1], 1000)
+    power_test_func = CubicSpline(t_sunrise, power)
+
+    Tavg, Tfunc, Terrfunc = get_T(t_sunrise, power, Tc=Tc)
+    Tavg_ls.append(Tavg)
+    Tfunc_ls.append(Tfunc)
+    Terrfunc_ls.append(Terrfunc)
+
+t_plot = np.linspace(t_sunrise[0], t_sunrise[-1])
+for i, func in enumerate(Tfunc_ls):
+    y = func(t_plot)/Tc
+    y = np.array(y, dtype=float)
+    plt.plot(t_plot, y, label=f'R={R_list[i]}')
+    yerr = Terrfunc_ls[i](t_plot)
+    yerr = np.array(yerr, dtype=float)
+    plt.fill_between(t_plot, y-yerr, y+yerr, alpha=0.5)
+plt.xlabel('Time')
+plt.ylabel(r'$T/T_{c}$')
+plt.legend()
+plt.show()
+
+for i, Tavg in enumerate(Tavg_ls):
+    plt.plot(R_list[i], Tavg, '.')
+    plt.xlabel('R')
+    plt.ylabel(r'$T_{avg}$')
+
+t_plot = np.linspace(t_sunrise[0], t_sunrise[-1])
+for i, func in enumerate(Tfunc_ls):
+    plt.plot(t_plot, 1-Tc/func(t_plot), label=f'R={R_list[i]}')
+plt.xlabel('Time')
+plt.ylabel(r'$\eta_{carnot}$')
+plt.legend()
+plt.show()
